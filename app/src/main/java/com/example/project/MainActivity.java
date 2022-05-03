ccbb958b80420f50be157a1d2de40ea43101f522
b21mihpa <b21mihpa@student.his.se>
2022-05-03 09:18:59 +0200

    added: ability to read/write shared preferences

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        sharedPref = getPreferences(MODE_PRIVATE);
+        editor = sharedPref.edit();
+
+        editor.putString("hello_world", "Hello, world!");
+        editor.apply();
+
+        Log.d("look_for_me", sharedPref.getString("hello_world", "Could not retrieve content."));
+    }
 }
